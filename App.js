94396c2470b90fafbbc42806ab5efc82e04d0185
TranDoanTriet <trandoantriet@gmail.com>
2022-12-13 21:43:26 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>hello this is my first mobile project</Text>
+      <Button title='suck me motherfucker'/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
